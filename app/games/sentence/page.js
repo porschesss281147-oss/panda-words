c40c5473fc4b0dbf9611a90f54be69dbc95b5006
Aphidech Phonsit <porschesss281147@gmail.com>
2026-03-09 07:51:37 +0700

    Update page.js

--- app/games/sentence/page.js
+++ app/games/sentence/page.js
@@ -559,7 +559,7 @@ export default function SentenceGamePage() {
     <div className="fixed inset-0 -z-10">
      <div 
   className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-  style={{ backgroundImage: `url('/panda-words/3.png')` }} 
+  style={{ backgroundImage: `url('/panda-words/cc.png')` }} 
 ></div>
       <div className="absolute inset-0 bg-white/30" />
     </div>
